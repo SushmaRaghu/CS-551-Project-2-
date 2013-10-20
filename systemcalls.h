d2746a7783412ade00b2cece10eb81ad2a8f04e2
SushmaRaghu <sushma.raghu@gmail.com>
2013-10-20 12:13:55 +0530

    Update systemcalls.h

--- systemcalls.h
+++ systemcalls.h
@@ -8,20 +8,18 @@
 
 
  int IG_Lookup(int groups[]){
-    message m;  
-   //m.m7_p1 = groups;
-
-    return(_syscall( PM_PROC_NR, IGLOOKUP, &m ));
-
-}
+     message m;
+//     m.m7_p1 = groups;
+     return(_syscall( PM_PROC_NR, IGLOOKUP, &m ));
+ }
 
   int IG_Create(int * groupId){
  
-  message m;
+      message m;
 
-	//m.m7_p1 = groupId;
+      m.m7_p1 = groupId;
     
-	return (_syscall( PM_PROC_NR, IGCREATE, &m ));
+      return (_syscall( PM_PROC_NR, IGCREATE, &m ));
 }
 
  int IG_Delete(int groupNo){
@@ -33,52 +31,38 @@ message m;
 
 }
 
- int IG_Publisher(int groupNo){
+ int IG_Publisher(int groupNo,int publisher_id){
 message m;
    m.m7_i1=(int)groupNo;
-   m.m7_i2 = (pid_t)getpid();
+     m.m7_i2 = (int) publisher_id;
    return( _syscall( PM_PROC_NR, IGPUBLISHER, &m ) );
 
 }
 
- int IG_Subscriber(int groupNo){
- message m;
+ int IG_Subscriber(int groupNo,int subscriber_id){
+     message m;
 	m.m7_i1=groupNo;
-   m.m7_i2 = (pid_t)getpid();
+     m.m7_i2 = (int)  subscriber_id;
    return( _syscall( PM_PROC_NR, IGSUBSCRIBER, &m ) );
 }
 
- int IG_Publish(int groupNo, char *msg){
+ int IG_Publish(int groupNo,int publisher_id, char *msg){
 message m;
    char *p=msg;
    m.m7_i1 = groupNo;
-   m.m7_i2 = (pid_t)getpid();
+     m.m7_i2 = publisher_id;
    m.m7_p1 = msg;
    return( _syscall( PM_PROC_NR, IGPUBLISH, &m ) );
 
 }
 
- int IG_Retreive(int groupNo, char *msg){
+ int IG_Retreive(int groupNo,int subscriber_id, char *msg){
  message m;	
    m.m7_i1 = groupNo;
    m.m7_p1 = msg;
-   m.m7_i2 = (pid_t)getpid();
+     m.m7_i2 = subscriber_id;
    return(_syscall( PM_PROC_NR, IGRETRIEVE, &m ));
 
 }
 
- int leave_Group_Subsrciber(int groupNo){
-message m;
-	//m.m7_i1 = groupNo;
-	//m.m7_i2 = getpid();
-	return (_syscall( PM_PROC_NR, 106, &m ));
-}
-
- int leave_Group_Publisher(int groupNo){
-message m;
-	//m.m7_i1 = groupNo;
-	//m.m7_i2 = getpid();
-   	return(_syscall( PM_PROC_NR, 105, &m ));
-
-}
-
+ 
