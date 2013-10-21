3a4fb95c51606fc48f442bce9c39ffee5853eb5b
SushmaRaghu <sushma.raghu@gmail.com>
2013-10-21 09:38:15 +0530

    Update entry_file.c

--- entry_file.c
+++ entry_file.c
@@ -15,17 +15,21 @@ int delete_a_group();
 
 int main(){
 
+//    while (true) {
+    
 int option ;
 
 printf("Hi.. Welcome to the application\n");
 printf("Here is a list of things that can be done in the application: For any option, please enter the group_id\n");
-printf("1. Can look up for the existing groups\n");
-printf("2. Create a group\n");
+printf("1. Create a group \n");
+printf("2. Can look up for the existing groups\n");
 printf("3. Can subscribe as a client for an existing group\n");
 printf("4. Can become a publisher for a group\n");
 printf("5. Can send a message\n");
 printf("6. Can receive a message\n");
 printf("7. Delete a group \n");
+printf("8. Reset MINIX\n");
+printf("9. Exit\n");
 
 
 printf("Please enter your option\n");
@@ -33,28 +37,28 @@ scanf("%d", &option);
 
 
 switch(option){
-case 1: look_up_for_a_group(); break;
-case 2: create_a_group();break;
+case 1: create_a_group(); break;
+case 2: look_up_for_a_group();break;
 case 3: act_as_a_subscriber();break;
 case 4: act_as_a_publisher();break;
 case 5: publish();break;
 case 6: retreive();break;
 case 7: delete_a_group();break;
+case 8: reset();break;
+case 9: exit(0);
 default:  break;
 
 }
-}
+    }
+//}
 
-      
+
 
 int create_a_group(){
 int group_id;
 
 int newGroup;
-
- 
-
-newGroup = IG_Create(&group_id);
+    newGroup = IG_Create(&group_id);
     if(newGroup == -1) {
            printf("Creation of a new interest group failed\n");
            return -1;
@@ -68,13 +72,10 @@ newGroup = IG_Create(&group_id);
 };
 
 int look_up_for_a_group(){
+
 int group_id;
-
-int newGroup;
-
- 
-
-newGroup = IG_Lookup(&group_id);
+    int newGroup;
+newGroup =  IG_Lookup(&group_id);
     if(newGroup == -1) {
            printf("Look up of interest group failed\n");
            return -1;
@@ -82,26 +83,33 @@ newGroup = IG_Lookup(&group_id);
            printf("Look up of interest group succeeded\n");
 		   return 1;
         }
-
-    
-
 };
 
 int act_as_a_publisher(){
 int group_id;
+    int publisher_id = 0;
+    char *choice;
 
 int newGroup1;
-int newGroup2;
-
-
- 
+    int newGroup2;
 newGroup1 = IG_Lookup(&group_id);
-newGroup2 = IG_Publisher(group_id);
+    printf("\nEnter group Id for which you want to be publisher:");
+    scanf("%d",&group_id);
+    printf("\nDo you have a publisher_id?(y/n)");
+    scanf("%s",choice);
+    if(strcmp(choice,"y") == 0){
+        printf("\nEnter publisher id:");
+        scanf("%d",&publisher_id);
+    }
+    
+
+
+newGroup2 = IG_Publisher(group_id,publisher_id);
     if(newGroup2 == -1) {
            printf("Registering as a publiseher failed\n");
            return -1;
         }else{
-           printf("Registering as a publiseher failed\n");
+           printf("Registering as a publiseher succeeded\n");
 		   return 1;
         }
 
@@ -111,13 +119,19 @@ newGroup2 = IG_Publisher(group_id);
 
 int act_as_a_subscriber(){
 int group_id;
-
 int newGroup;
+    char *choice;
+    int subscriber_id =0;
+    printf("\nEnter group Id for which you want to be subscriber:");
+    scanf("%d",&group_id);
+    printf("\nDo you have a subsciber_id?(y/n)");
+    scanf("%s",choice);
+    if(strcmp(choice,"y") == 0){
+        printf("\nEnter subscriber id:");
+        scanf("%d",&subscriber_id);
+    }
 
-
- 
- 
-newGroup = IG_Subscriber(group_id);
+    newGroup = IG_Subscriber(group_id,subscriber_id);
     if(newGroup == -1) {
            printf("Registering as a subscribr failed\n");
            return -1;
@@ -125,21 +139,21 @@ newGroup = IG_Subscriber(group_id);
            printf("Registering as a subscriber succeeded\n");
 		   return 1;
         }
-
-    
-
 };
 
 
 int publish(){
-int group_id;
+int group_id,publisher_id;
 
 int newGroup;
 char message[200] ={0};
-
-  
- 
-newGroup = IG_Publish(group_id,message);
+    printf("\nEnter your publisher id");
+    scanf("%d",&publisher_id);
+    printf("\nEnter the group id you wish to publish to:");
+    scanf("%d",&group_id);
+    printf("\nEnter the message you wish to publish");
+    scanf("%s",message);
+    newGroup = IG_Publish(group_id,publisher_id,message);
     if(newGroup == -1) {
            printf("Publishing message failed\n");
            return -1;
@@ -153,14 +167,18 @@ newGroup = IG_Publish(group_id,message);
 };
 
 int retreive(){
-int group_id;
+int group_id,subscriber_id;
 
 int newGroup;
 char message[200]={0};
 
+    printf("\nEnter your subscriber id");
+    scanf("%d",&subscriber_id);
+    printf("\nEnter the group id from which u want to receive from:");
+    scanf("%d",&group_id);
+    
  
- 
-newGroup = IG_Retreive(group_id,message);
+newGroup = IG_Retreive(group_id,subscriber_id,message);
     if(newGroup == -1) {
            printf("Rx message failed\n");
            return -1;
@@ -177,6 +195,8 @@ int delete_a_group(){
 int group_id;
 
 int newGroup;
+    printf("\nEnter group Id you want to delete:");
+    scanf("%d",&group_id);
 
  
  
@@ -193,6 +213,21 @@ newGroup = IG_Delete(group_id);
 
 };
 
+int reset(){
 
+int newGroup;
+
+newGroup = IG_getBackToOriginalMINIX();
+ if(newGroup == -1) {
+           printf("reset failed\n");
+           return -1;
+        }else{
+           printf("reset succeeded\n");
+		   return 1;
+        }
+
+
+
+}
     
 
